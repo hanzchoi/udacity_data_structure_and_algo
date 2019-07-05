6fa65adff0915c403ca1dfe24b6097e0c39453db
Hanz <personabe1984@gmail.com>
2019-07-04 20:30:15 -0400

    solved Task0 as they instructed

--- Project 1/P0/Task0.py
+++ Project 1/P0/Task0.py
@@ -22,5 +22,19 @@ Print messages:
 
 # print("First record of texts, <incoming number> texts <answering number> at time <time>")
 # print("Last record of calls, <incoming number> calls <answering number> at time <time>, lasting <during> seconds")
-print("text: ", texts[0])
-print("Call: ", calls[-1])
+
+
+text_incoming_number = texts[0][0]
+text_answering_number = texts[0][1]
+text_time = texts[0][2]
+
+calls_incoming_number = calls[-1][0]
+calls_answering_number = calls[-1][1]
+calls_time = calls[-1][2]
+calls_during = calls[-1][3]
+
+print(texts[0])
+print(calls[-1])
+print('First record of texts, {} texts {} at time {}'.format(text_incoming_number, text_answering_number, text_time))
+print('Last record of calls, {} calls {} at time {}, lasting {} seconds'.format(calls_incoming_number, calls_answering_number, calls_time, calls_during))
+
