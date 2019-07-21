eeb44cc6e234beed8eeb85dd394ef66b0dc4909e
Hanz <personabe1984@gmail.com>
2019-07-21 18:06:51 -0400

    added is_leap_year function for the days_between_dates file and the logic is from wikipedia

--- introduction/lesson3/days_between_dates.py
+++ introduction/lesson3/days_between_dates.py
@@ -7,6 +7,17 @@
 #
 
 
+def is_leap_year(year):
+    if year % 400 == 0:
+        return True
+    elif year % 100 == 0:
+        return False
+    elif year % 4 == 0:
+        return True
+    else:
+        return False
+
+
 def days_in_month(year, month):
     # if month in (1, 3, 5, 7, 8, 10, 12)  this is to use a
     if month == 1 or month == 3 or month == 5 or month == 7 \
@@ -14,7 +25,10 @@ def days_in_month(year, month):
         return 31
     else:
         if month == 2:
-            return 28
+            if is_leap_year(year):
+                return 29
+            else:
+                return 28
         else:
             return 30
 
