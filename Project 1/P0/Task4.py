5de1b8c7b893d5397cca417fa4583cf0cbf587b1
Hanz <personabe1984@gmail.com>
2019-07-26 15:18:09 -0400

    started to work on Task4 on P0. I listed out crude version of pseudo code for this problem.

--- Project 1/P0/Task4.py
+++ Project 1/P0/Task4.py
@@ -12,10 +12,6 @@ with open('calls.csv', 'r') as f:
     reader = csv.reader(f)
     calls = list(reader)
 
-with open('calls_01.csv', 'r') as f:
-    reader = csv.reader(f)
-    calls01 = list(reader)
-
 """
 TASK 4:
 The telephone company want to identify numbers that might be doing
@@ -29,3 +25,27 @@ Print a message:
 The list of numbers should be print out one per line in lexicographic order with no duplicates.
 """
 
+"""
+First get the telemarketing numbers from the phone number list and this time only the out call number 
+I am guessing that it is not the usual numbers that starts with 140? 
+If that is the case then make a set of outcall incoming call out going text incoming text
+combine all of the incoming numbers, text_sending numbers, text_receiving_numbers
+"""
+
+def get_outcall_numbers(call_log):
+
+def get_incoming_numbers(call_log):
+
+def get_sending_text_numbers(text_log):
+    
+def get_receive_text_numbers(text_log):
+
+
+print('These numbers could be telemarketers: ')
+# list of numbers
+
+call_set = set([])
+
+for call_log in calls:
+    call_set.add(call_log[0])
+    call_set.add(call_log[1])
