c7a3252c591c5ebb4f133168d22c86cf9333392e
Hanz <personabe1984@gmail.com>
2019-07-31 19:49:41 -0400

    cleaned up task0 in P0. Now it only needs two lines of code

--- Project 1/P0/Task0.py
+++ Project 1/P0/Task0.py
@@ -20,18 +20,5 @@ Print messages:
 "Last record of calls, <incoming number> calls <answering number> at time <time>, lasting <during> seconds"
 """
 
-
-text_incoming_number = texts[0][0]
-text_answering_number = texts[0][1]
-text_time = texts[0][2]
-
-calls_incoming_number = calls[-1][0]
-calls_answering_number = calls[-1][1]
-calls_time = calls[-1][2]
-calls_during = calls[-1][3]
-
-print(texts[0])
-print(calls[-1])
-print('First record of texts, {} texts {} at time {}'.format(text_incoming_number, text_answering_number, text_time))
-print('Last record of calls, {} calls {} at time {}, lasting {} seconds'.format(calls_incoming_number, calls_answering_number, calls_time, calls_during))
-
+print('First record of texts, {0} texts {1} at time {2}'.format(*texts[0]))
+print('Last record of calls, {0} calls {1} at time {2}, lasting {3} seconds'.format(*calls[-1]))
