bb74ed7ab11c8b60b07821e26b9d8e19a3235857
Hanz <personabe1984@gmail.com>
2019-08-03 20:50:39 -0400

    added to_list in linked list class to test it

---
+++ Practice/linked_lists/linked_list.py
@@ -0,0 +1,54 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, value):
+        if self.head is None:
+            self.head = Node(value)
+            return
+
+        # Move to the tail (the last node)
+        node = self.head
+        while node.next:
+            node = node.next
+
+        node.next = Node(value)
+        return
+
+    # TODO: Write function to turn Linked List into Python List
+    def to_list(self):
+        our_list = []
+
+        current = self.head
+        while current:
+            our_list.append(current.value)
+            current = current.next
+
+        return our_list
+
+
+linked_list = LinkedList()
+linked_list.append(1)
+linked_list.append(2)
+linked_list.append(4)
+
+node = linked_list.head
+while node:
+    print(node.value)
+    node = node.next
+
+
+# Test your method here
+linked_list = LinkedList()
+linked_list.append(3)
+linked_list.append(2)
+linked_list.append(-1)
+linked_list.append(0.2)
+
+print("Pass" if (linked_list.to_list() == [3, 2, -1, 0.2]) else "Fail")
