da07d1594659b652921320f42276dc0cc1240d70
Hanz <personabe1984@gmail.com>
2019-08-03 20:00:00 -0400

    added linked list practice for node

--- Practice/linked_lists/node.py
+++ Practice/linked_lists/node.py
@@ -16,3 +16,88 @@ print(head.next.value)
 print(head.next.next.value)
 print(head.next.next.next.value)
 print(head.next.next.next.next.value)
+
+
+def print_linked_list(head):
+    current_node = head
+
+    while current_node is not None:
+        print(current_node.value)
+        current_node = current_node.next
+
+
+print_linked_list(head)
+
+"""
+    See if you can write the code for the create_linked_list function below
+    The function should take a Python list of values as input and return the head of a linked list that has those values
+"""
+
+
+def create_linked_list(input_list):
+    """
+    Function to create a linked list
+    @param input_list: a list of integers
+    @return: head node of the linked list
+    """
+    head = None
+
+    for value in input_list:
+        if head is None:
+            head = Node(value)
+        else:
+            # Move to the tail (the last node)
+            current_node = head
+            while current_node.next:
+                current_node = current_node.next
+
+            current_node.next = Node(value)
+
+    return head
+
+
+def create_linked_list_better(input_list):
+    head = None
+    tail = None
+
+    for value in input_list:
+
+        if head is None:
+            head = Node(value)
+            tail = head  # when we only have 1 node, head and tail refer to the same node
+        else:
+            tail.next = Node(value)  # attach the new node to the `next` of tail
+            tail = tail.next  # update the tail
+
+    return head
+
+
+### Test Code
+def test_function(input_list, head):
+    try:
+        if len(input_list) == 0:
+            if head is not None:
+                print("Fail")
+                return
+        for value in input_list:
+            if head.value != value:
+                print("Fail")
+                return
+            else:
+                head = head.next
+        print("Pass")
+    except Exception as e:
+        print("Fail: " + e)
+
+
+input_list = [1, 2, 3, 4, 5, 6]
+head = create_linked_list_better(input_list)
+test_function(input_list, head)
+
+input_list = [1]
+head = create_linked_list_better(input_list)
+test_function(input_list, head)
+
+input_list = []
+head = create_linked_list_better(input_list)
+test_function(input_list, head)
